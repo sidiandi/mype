9dcee4155b1b5474b7bc84f79ab8fc5fe98769b5
Andreas Grimme <andreas.grimme@siemens.com>
2019-05-05 11:41:35 +0200

    diskpart script

--- run.cmd
+++ run.cmd
@@ -1,4 +1,4 @@
 echo Hello, Winpe!
 diskpart /s %~dp0diskpart.txt
 copy d:\sources\boot.wim M:\
-DISM.exe /Apply-Image /ImageFile:M:\boot.wim /ApplyDrive:C:\ /index:1
+DISM.exe /Apply-Image /ImageFile:M:\boot.wim /ApplyDir:C:\ /index:1
